70c5d39c6cfef3219d233f4af658a8c3251a9fb5
thentgesMindee <thibault@mindee.co>
2022-10-17 17:55:48 +0200

    chg: :recycle: refactor RedisBackend & RedisSentinelBackend to use _RedisBackend

--- fastapi_cache/backends/redis.py
+++ fastapi_cache/backends/redis.py
@@ -6,49 +6,36 @@ from redis.asyncio.sentinel import Sentinel
 from fastapi_cache.backends import Backend
 
 
-class RedisBackend(Backend):
+class _RedisBackend(Backend):
+    def __init__(self, writer: Redis, reader: Redis):
+        self.writer = writer
+        self.reader = reader
+
+    async def get_with_ttl(self, key: str) -> Tuple[int, str]:
+        async with self.reader.pipeline(transaction=True) as pipe:
+            return await (pipe.ttl(key).get(key).execute())
+
+    async def get(self, key) -> str:
+        return await self.reader.get(key)
+
+    async def set(self, key: str, value: str, expire: int = None):
+        return await self.writer.set(key, value, ex=expire)
+
+    async def clear(self, namespace: str = None, key: str = None) -> int:
+        if namespace:
+            lua = f"for i, name in ipairs(redis.call('KEYS', '{namespace}:*')) do redis.call('DEL', name); end"
+            return await self.writer.eval(lua, numkeys=0)
+        elif key:
+            return await self.writer.delete(key)
+
+
+class RedisBackend(_RedisBackend):
     def __init__(self, redis: Redis):
-        self.redis = redis
-
-    async def get_with_ttl(self, key: str) -> Tuple[int, str]:
-        async with self.redis.pipeline(transaction=True) as pipe:
-            return await (pipe.ttl(key).get(key).execute())
-
-    async def get(self, key) -> str:
-        return await self.redis.get(key)
-
-    async def set(self, key: str, value: str, expire: int = None):
-        return await self.redis.set(key, value, ex=expire)
-
-    async def clear(self, namespace: str = None, key: str = None) -> int:
-        if namespace:
-            lua = f"for i, name in ipairs(redis.call('KEYS', '{namespace}:*')) do redis.call('DEL', name); end"
-            return await self.redis.eval(lua, numkeys=0)
-        elif key:
-            return await self.redis.delete(key)
+        super().__init__(writer=redis, reader=redis)
 
 
-class RedisSentinelBackend(Backend):
+class RedisSentinelBackend(_RedisBackend):
     def __init__(self, sentinel: Sentinel, service_name: str):
-        self.sentinel = sentinel
-        self.service_name = service_name
-
-        self.master = self.sentinel.master_for(self.service_name)
-        self.slave = self.sentinel.slave_for(self.service_name)
-
-    async def get_with_ttl(self, key: str) -> Tuple[int, str]:
-        async with self.slave.pipeline(transaction=True) as pipe:
-            return await (pipe.ttl(key).get(key).execute())
-
-    async def get(self, key) -> str:
-        return await self.slave.get(key)
-
-    async def set(self, key: str, value: str, expire: int = None):
-        return await self.master.set(key, value, ex=expire)
-
-    async def clear(self, namespace: str = None, key: str = None) -> int:
-        if namespace:
-            lua = f"for i, name in ipairs(redis.call('KEYS', '{namespace}:*')) do redis.call('DEL', name); end"
-            return await self.master.eval(lua, numkeys=0)
-        elif key:
-            return await self.master.delete(key)
+        super().__init__(
+            writer=sentinel.master_for(service_name), reader=sentinel.slave_for(service_name)
+        )
