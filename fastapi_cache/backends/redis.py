a02c08f0f0481335b461930adf8f34c9b1804f27
Thibault <47983979+thentgesMindee@users.noreply.github.com>
2022-10-19 14:07:21 +0200

    new: :sparkles: add support for redis-sentinel (#1)
    * new: :sparkles: redis-sentinel support (RedisSentinelBackend)
    
    * chg: :recycle: refactor RedisBackend & RedisSentinelBackend to use _RedisBackend
    
    * fix: :bug: ensure_async_func | add default value to .pop

--- fastapi_cache/backends/redis.py
+++ fastapi_cache/backends/redis.py
@@ -1,27 +1,41 @@
 from typing import Tuple
 
 from redis.asyncio.client import Redis
+from redis.asyncio.sentinel import Sentinel
 
 from fastapi_cache.backends import Backend
 
 
-class RedisBackend(Backend):
-    def __init__(self, redis: Redis):
-        self.redis = redis
+class _RedisBackend(Backend):
+    def __init__(self, writer: Redis, reader: Redis):
+        self.writer = writer
+        self.reader = reader
 
     async def get_with_ttl(self, key: str) -> Tuple[int, str]:
-        async with self.redis.pipeline(transaction=True) as pipe:
+        async with self.reader.pipeline(transaction=True) as pipe:
             return await (pipe.ttl(key).get(key).execute())
 
     async def get(self, key) -> str:
-        return await self.redis.get(key)
+        return await self.reader.get(key)
 
     async def set(self, key: str, value: str, expire: int = None):
-        return await self.redis.set(key, value, ex=expire)
+        return await self.writer.set(key, value, ex=expire)
 
     async def clear(self, namespace: str = None, key: str = None) -> int:
         if namespace:
             lua = f"for i, name in ipairs(redis.call('KEYS', '{namespace}:*')) do redis.call('DEL', name); end"
-            return await self.redis.eval(lua, numkeys=0)
+            return await self.writer.eval(lua, numkeys=0)
         elif key:
-            return await self.redis.delete(key)
+            return await self.writer.delete(key)
+
+
+class RedisBackend(_RedisBackend):
+    def __init__(self, redis: Redis):
+        super().__init__(writer=redis, reader=redis)
+
+
+class RedisSentinelBackend(_RedisBackend):
+    def __init__(self, sentinel: Sentinel, service_name: str):
+        super().__init__(
+            writer=sentinel.master_for(service_name), reader=sentinel.slave_for(service_name)
+        )
